3ffcd607916f364304a3441906dfde4c8cd6837d
Rabby <fajla.rabby008@gmail.com>
2022-04-05 23:22:17 +0600

    blogs done

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,10 +1,73 @@
 import React from 'react';
+import { Bar, BarChart, CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import "./Dashboard.css";
 
 const Dashboard = () => {
+   const data = [
+        {
+            "month": "Mar",
+            "investment": 100000,
+            "sell": 241,
+            "revenue": 10401
+        },
+        {
+            "month": "Apr",
+            "investment": 200000,
+            "sell": 423,
+            "revenue": 24500
+        },
+        {
+            "month": "May",
+            "investment": 500000,
+            "sell": 726,
+            "revenue": 67010
+        },
+        {
+            "month": "Jun",
+            "investment": 500000,
+            "sell": 529,
+            "revenue": 40405
+        },
+        {
+            "month": "Jul",
+            "investment": 600000,
+            "sell": 601,
+            "revenue": 50900
+        },
+        {
+            "month": "Aug",
+            "investment": 700000,
+            "sell": 670,
+            "revenue": 61000
+        }
+    ]
     return (
-        <div>
-            <h2>DashBoard!!</h2>
-        </div>
+        
+       <div className='charts'>
+                        <div>
+                        <LineChart width={600} height={300} data={data}>
+                    <Line type="monotone" dataKey="sell" stroke="#8884d8" />
+                    <CartesianGrid stroke="#ccc" />
+                    <XAxis dataKey="month" />
+                    {/* <YAxis dataKey="month"/> */}
+                    <Tooltip />
+                </LineChart>
+                            </div>
+
+
+
+                <div>
+                                    <BarChart width={600} height={300} data={data}>
+                            <XAxis dataKey="month" stroke="#8884d8" />
+                            <YAxis />
+                            <Tooltip wrapperStyle={{ width: 100, backgroundColor: '#ccc' }} />
+                            <Legend width={100} wrapperStyle={{ top: 40, right: 20, backgroundColor: '#f5f5f5', border: '1px solid #d5d5d5', borderRadius: 3, lineHeight: '40px' }} />
+                            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                            <Bar dataKey="sell" fill="#8884d8" barSize={30} />
+                        </BarChart>
+                </div>
+       </div>
+        
     );
 };
 
--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -4,7 +4,24 @@ import "./NotFound.css";
 
 const NotFound = () => {
     return (
-        <h1 className='four'>404</h1>
+        // <h1 className='four'>404</h1>
+        <section class="page_404">
+	<div class="container">
+		<div class="row">	
+		<div class="col-sm-12 ">
+		<div class="col-sm-10 col-sm-offset-1  text-center">
+		<div class="four_zero_four_bg">
+			<h1 class="text-center ">404 Error</h1>
+		
+		
+		</div>
+		
+		
+		</div>
+		</div>
+		</div>
+	</div>
+</section>
      
   
 
